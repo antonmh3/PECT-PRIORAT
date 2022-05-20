6ab549ddcfa1505acad45e155f98160ddd8056c6
antonmh3 <antonmh2000@gmail.com>
2022-05-20 03:10:28 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ jQuery('document').ready(function($){
 	var menuB = $('.menu'),
 		menu= $('.navigation ul');
 
+/*condicional mosta o no mostra el contingut de la capçalera*/
+
 	menuB.click(function(){
 
 	if(menu.hasClass ('show')){
